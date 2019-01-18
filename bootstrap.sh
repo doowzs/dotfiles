b1ecb18904bbc74b60ea027b21a5b54ffd1ae4ad
Tianyun Zhang <doowzs@qq.com>
2019-01-18 12:31:04 +0800

    add root privilege check

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run this script as root."
+    exit
+fi
+
 echo "=================================="
 echo "Update apt and install ansible."
 echo "=================================="
